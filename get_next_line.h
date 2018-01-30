a50c37850ddc6867ab7bc952d67d8dabe55b5617
Pacôme Françon <pacome.francon@epitech.eu>
2018-01-30 15:59:59 +0100

    100% des tests

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** get_next_line
+** File description:
+** get_next_line
+*/
+
+#pragma once
+
+char *get_next_line(int fd);
+
+#ifndef READ_SIZE
+	#define READ_SIZE (1)
+#endif
